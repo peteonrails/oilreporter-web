ad62f695a34a76fb4a33f0ec27e8fa3b987628ef
ssoper <sean.soper@gmail.com>
2010-05-13 16:39:59 -0400

    merging routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ ActionController::Routing::Routes.draw do |map|
   # Reports
   map.resources :reports, :only => [:create, :update, :index, :map]
   map.connect '/map', :controller => 'reports', :action => 'map'
-  
+
+  # Developers
+  map.resources :developers, :only => [:new, :create]
+  map.connect '/signup', :controller => 'developers', :action => 'new'
+
   # Documentation
   map.api '/api', :controller => 'api', :action => 'index'
   

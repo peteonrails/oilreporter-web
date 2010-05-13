d0cdd5adc1c9045f0b03863809885a9e9533ec6f
Brendan G. Lim <brendangl@gmail.com>
2010-05-13 12:51:50 -0400

    Adding in the actual migration for organizataions

--- db/migrate/20100513164344_add_organization_id_to_reports.rb
+++ db/migrate/20100513164344_add_organization_id_to_reports.rb
@@ -1,7 +1,9 @@
 class AddOrganizationIdToReports < ActiveRecord::Migration
   def self.up
+    add_column :reports, :organization_id, :string
   end
 
   def self.down
+    remove_column :reports, :organization_id
   end
 end

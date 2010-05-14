cf59a03fd651023003a6c571b44ed966e0946aa2
Brendan G. Lim <brendangl@gmail.com>
2010-05-14 16:07:12 -0400

    Flash erorrs

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -14,7 +14,6 @@ class DevelopersController < ApplicationController
       redirect_to :action => 'show', :id => Base64.encode64(@developer.email).strip
     else
       flash[:error] = "There was a problem trying to create your API key"
-      flash[:error_messages_for] = @developer.errors
       render :new
     end
   end

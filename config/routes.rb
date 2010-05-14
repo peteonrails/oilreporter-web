d8d50d4a53fe07d5e1db0304d924ad32f338d638
Jonathan Nelson <jonathan@intridea.com>
2010-05-14 15:23:43 -0400

    fix for favicon

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   # Home
   map.root :controller => 'home'
   map.api '/api', :controller => 'home', :action => 'api'
+  map.favicon '*favicon.ico', :controller => 'home'
   
   # Reports
   map.resources :reports, :only => [:create, :update, :index, :map]

eef3a3a95ae9170ca5130c1953e32bf63887c9d9
Brendan G. Lim <brendangl@gmail.com>
2010-06-16 18:50:26 -0400

    Fixing ReportSession naming

--- app/controllers/report_sessions_controller.rb
+++ app/controllers/report_sessions_controller.rb
@@ -1,9 +1,8 @@
-class ReportsController < ApplicationController
+class ReportSessionsController < ApplicationController
   before_filter :verify_api_key, :only => [:create]
 
   def create
     report_session = ReportSession.create
-
     if report_session.save
       render :json => { :id => report_session.id }, :status => :ok
     else
